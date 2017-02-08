83ffb79240784296032c0cd170b5f66c2aa9cc7b
dllo <dllo@dllodeMac-mini-294.local>
2017-02-08 10:55:05 +0800

    第一次提交的简单版本

---
+++ component/HomeDetail.js
@@ -0,0 +1,60 @@
+
+
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+var HomeDetail = React.createClass({
+    getInitialState:function () {
+        return{
+            summary:''
+        }
+    },
+    render:function () {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    {this.state.summary}
+                </Text>
+            </View>
+        );
+    },
+    componentDidMount:function () {
+        var url_api = "http://api.douban.com/v2/movie/subject/"+this.props.data.id;
+        //网络请求
+        fetch(url_api)
+            .then((response)=>response.json())
+            .then((responseData)=>{
+                var summary = responseData.summary;
+                this.setState({
+                   summary:summary
+                });
+            })
+            .catch((error)=>{
+                if(error){
+                    alert("错误");
+                }
+            });
+    }
+});
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    }
+});
+
+module.exports = HomeDetail;
